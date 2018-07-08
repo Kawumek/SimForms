468843f42fb3f3680fbe134cd0317ce59657b2c7
Сергей Камушкин <kawumek@gmail.com>
2018-07-08 22:10:55 +0400

    Add project files.

---
+++ MainForm.Designer.cs
@@ -0,0 +1,405 @@
+﻿namespace SimForms
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SupermarketCButton = new System.Windows.Forms.Button();
+            this.HouseCButton = new System.Windows.Forms.Button();
+            this.HouseDButton = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.RewriterButton = new System.Windows.Forms.Button();
+            this.CassierButton = new System.Windows.Forms.Button();
+            this.SupermarketDButton = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.VehiclesButton = new System.Windows.Forms.Button();
+            this.AnalystButton = new System.Windows.Forms.Button();
+            this.button13 = new System.Windows.Forms.Button();
+            this.SchoolButton = new System.Windows.Forms.Button();
+            this.UniversityButton = new System.Windows.Forms.Button();
+            this.HealthProgress = new System.Windows.Forms.ProgressBar();
+            this.HungerProgress = new System.Windows.Forms.ProgressBar();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.CloseButton = new System.Windows.Forms.Button();
+            this.NextDayButton = new System.Windows.Forms.Button();
+            this.SaveButton = new System.Windows.Forms.Button();
+            this.MoneyBox = new System.Windows.Forms.TextBox();
+            this.DayBox = new System.Windows.Forms.TextBox();
+            this.TimeBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // SupermarketCButton
+            // 
+            this.SupermarketCButton.Location = new System.Drawing.Point(12, 607);
+            this.SupermarketCButton.Name = "SupermarketCButton";
+            this.SupermarketCButton.Size = new System.Drawing.Size(151, 80);
+            this.SupermarketCButton.TabIndex = 0;
+            this.SupermarketCButton.Text = "Дешевый супермаркет";
+            this.SupermarketCButton.UseVisualStyleBackColor = true;
+            this.SupermarketCButton.Click += new System.EventHandler(this.SupermarketCButton_Click);
+            // 
+            // HouseCButton
+            // 
+            this.HouseCButton.Location = new System.Drawing.Point(12, 32);
+            this.HouseCButton.Name = "HouseCButton";
+            this.HouseCButton.Size = new System.Drawing.Size(108, 80);
+            this.HouseCButton.TabIndex = 1;
+            this.HouseCButton.Text = "Дешевый дом";
+            this.HouseCButton.UseVisualStyleBackColor = true;
+            this.HouseCButton.Click += new System.EventHandler(this.HouseCButton_Click);
+            // 
+            // HouseDButton
+            // 
+            this.HouseDButton.Location = new System.Drawing.Point(77, 469);
+            this.HouseDButton.Name = "HouseDButton";
+            this.HouseDButton.Size = new System.Drawing.Size(106, 80);
+            this.HouseDButton.TabIndex = 2;
+            this.HouseDButton.Text = "Обычный дом";
+            this.HouseDButton.UseVisualStyleBackColor = true;
+            this.HouseDButton.Click += new System.EventHandler(this.HouseDButton_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Black;
+            this.button2.Location = new System.Drawing.Point(61, 118);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(10, 483);
+            this.button2.TabIndex = 3;
+            this.button2.UseVisualStyleBackColor = false;
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.Black;
+            this.button3.Location = new System.Drawing.Point(61, 555);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(452, 10);
+            this.button3.TabIndex = 4;
+            this.button3.UseVisualStyleBackColor = false;
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.Black;
+            this.button4.Location = new System.Drawing.Point(412, 293);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(10, 268);
+            this.button4.TabIndex = 5;
+            this.button4.UseVisualStyleBackColor = false;
+            // 
+            // RewriterButton
+            // 
+            this.RewriterButton.Location = new System.Drawing.Point(113, 213);
+            this.RewriterButton.Name = "RewriterButton";
+            this.RewriterButton.Size = new System.Drawing.Size(138, 74);
+            this.RewriterButton.TabIndex = 6;
+            this.RewriterButton.Text = "Работа: Переписчик";
+            this.RewriterButton.UseVisualStyleBackColor = true;
+            // 
+            // CassierButton
+            // 
+            this.CassierButton.Location = new System.Drawing.Point(428, 357);
+            this.CassierButton.Name = "CassierButton";
+            this.CassierButton.Size = new System.Drawing.Size(118, 74);
+            this.CassierButton.TabIndex = 7;
+            this.CassierButton.Text = "Работа: Продавец";
+            this.CassierButton.UseVisualStyleBackColor = true;
+            // 
+            // SupermarketDButton
+            // 
+            this.SupermarketDButton.Location = new System.Drawing.Point(256, 449);
+            this.SupermarketDButton.Name = "SupermarketDButton";
+            this.SupermarketDButton.Size = new System.Drawing.Size(150, 100);
+            this.SupermarketDButton.TabIndex = 8;
+            this.SupermarketDButton.Text = "Обычный супермаркет";
+            this.SupermarketDButton.UseVisualStyleBackColor = true;
+            // 
+            // button8
+            // 
+            this.button8.BackColor = System.Drawing.Color.Black;
+            this.button8.Location = new System.Drawing.Point(503, 555);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(10, 132);
+            this.button8.TabIndex = 9;
+            this.button8.UseVisualStyleBackColor = false;
+            // 
+            // button9
+            // 
+            this.button9.BackColor = System.Drawing.Color.Black;
+            this.button9.Location = new System.Drawing.Point(342, 677);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(171, 10);
+            this.button9.TabIndex = 10;
+            this.button9.UseVisualStyleBackColor = false;
+            // 
+            // VehiclesButton
+            // 
+            this.VehiclesButton.Location = new System.Drawing.Point(347, 571);
+            this.VehiclesButton.Name = "VehiclesButton";
+            this.VehiclesButton.Size = new System.Drawing.Size(150, 100);
+            this.VehiclesButton.TabIndex = 11;
+            this.VehiclesButton.Text = "Авторынок";
+            this.VehiclesButton.UseVisualStyleBackColor = true;
+            this.VehiclesButton.Click += new System.EventHandler(this.VehiclesButton_Click);
+            // 
+            // AnalystButton
+            // 
+            this.AnalystButton.Location = new System.Drawing.Point(428, 449);
+            this.AnalystButton.Name = "AnalystButton";
+            this.AnalystButton.Size = new System.Drawing.Size(173, 100);
+            this.AnalystButton.TabIndex = 13;
+            this.AnalystButton.Text = "Работа: Аналитик акций";
+            this.AnalystButton.UseVisualStyleBackColor = true;
+            // 
+            // button13
+            // 
+            this.button13.BackColor = System.Drawing.Color.Black;
+            this.button13.Location = new System.Drawing.Point(61, 293);
+            this.button13.Name = "button13";
+            this.button13.Size = new System.Drawing.Size(431, 10);
+            this.button13.TabIndex = 14;
+            this.button13.UseVisualStyleBackColor = false;
+            // 
+            // SchoolButton
+            // 
+            this.SchoolButton.Location = new System.Drawing.Point(77, 309);
+            this.SchoolButton.Name = "SchoolButton";
+            this.SchoolButton.Size = new System.Drawing.Size(150, 100);
+            this.SchoolButton.TabIndex = 15;
+            this.SchoolButton.Text = "Школа";
+            this.SchoolButton.UseVisualStyleBackColor = true;
+            // 
+            // UniversityButton
+            // 
+            this.UniversityButton.Location = new System.Drawing.Point(342, 187);
+            this.UniversityButton.Name = "UniversityButton";
+            this.UniversityButton.Size = new System.Drawing.Size(150, 100);
+            this.UniversityButton.TabIndex = 16;
+            this.UniversityButton.Text = "Университет";
+            this.UniversityButton.UseVisualStyleBackColor = true;
+            // 
+            // HealthProgress
+            // 
+            this.HealthProgress.Location = new System.Drawing.Point(625, 12);
+            this.HealthProgress.Name = "HealthProgress";
+            this.HealthProgress.Size = new System.Drawing.Size(300, 38);
+            this.HealthProgress.Step = 0;
+            this.HealthProgress.Style = System.Windows.Forms.ProgressBarStyle.Continuous;
+            this.HealthProgress.TabIndex = 20;
+            // 
+            // HungerProgress
+            // 
+            this.HungerProgress.Location = new System.Drawing.Point(625, 56);
+            this.HungerProgress.Name = "HungerProgress";
+            this.HungerProgress.Size = new System.Drawing.Size(300, 38);
+            this.HungerProgress.Style = System.Windows.Forms.ProgressBarStyle.Continuous;
+            this.HungerProgress.TabIndex = 21;
+            // 
+            // label3
+            // 
+            this.label3.Location = new System.Drawing.Point(528, 12);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(91, 38);
+            this.label3.TabIndex = 22;
+            this.label3.Text = "Здоровье";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label4
+            // 
+            this.label4.Location = new System.Drawing.Point(519, 56);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(100, 38);
+            this.label4.TabIndex = 23;
+            this.label4.Text = "Сытость";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // CloseButton
+            // 
+            this.CloseButton.Location = new System.Drawing.Point(812, 647);
+            this.CloseButton.Name = "CloseButton";
+            this.CloseButton.Size = new System.Drawing.Size(113, 40);
+            this.CloseButton.TabIndex = 24;
+            this.CloseButton.Text = "Закрыть";
+            this.CloseButton.UseVisualStyleBackColor = true;
+            this.CloseButton.Click += new System.EventHandler(this.CloseButton_Click);
+            // 
+            // NextDayButton
+            // 
+            this.NextDayButton.Location = new System.Drawing.Point(683, 601);
+            this.NextDayButton.Name = "NextDayButton";
+            this.NextDayButton.Size = new System.Drawing.Size(242, 40);
+            this.NextDayButton.TabIndex = 25;
+            this.NextDayButton.Text = "Следующий день";
+            this.NextDayButton.UseVisualStyleBackColor = true;
+            this.NextDayButton.Click += new System.EventHandler(this.NextDayButton_Click);
+            // 
+            // SaveButton
+            // 
+            this.SaveButton.Location = new System.Drawing.Point(683, 647);
+            this.SaveButton.Name = "SaveButton";
+            this.SaveButton.Size = new System.Drawing.Size(123, 40);
+            this.SaveButton.TabIndex = 26;
+            this.SaveButton.Text = "Сохранить";
+            this.SaveButton.UseVisualStyleBackColor = true;
+            this.SaveButton.Click += new System.EventHandler(this.SaveButton_Click);
+            // 
+            // MoneyBox
+            // 
+            this.MoneyBox.Location = new System.Drawing.Point(775, 100);
+            this.MoneyBox.Name = "MoneyBox";
+            this.MoneyBox.ReadOnly = true;
+            this.MoneyBox.Size = new System.Drawing.Size(150, 38);
+            this.MoneyBox.TabIndex = 27;
+            // 
+            // DayBox
+            // 
+            this.DayBox.Location = new System.Drawing.Point(775, 144);
+            this.DayBox.Name = "DayBox";
+            this.DayBox.ReadOnly = true;
+            this.DayBox.Size = new System.Drawing.Size(150, 38);
+            this.DayBox.TabIndex = 28;
+            // 
+            // TimeBox
+            // 
+            this.TimeBox.Location = new System.Drawing.Point(775, 187);
+            this.TimeBox.Name = "TimeBox";
+            this.TimeBox.ReadOnly = true;
+            this.TimeBox.Size = new System.Drawing.Size(150, 38);
+            this.TimeBox.TabIndex = 29;
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(669, 99);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(100, 38);
+            this.label1.TabIndex = 30;
+            this.label1.Text = "Деньги";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // label2
+            // 
+            this.label2.Location = new System.Drawing.Point(669, 143);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(100, 38);
+            this.label2.TabIndex = 31;
+            this.label2.Text = "День";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // label5
+            // 
+            this.label5.Location = new System.Drawing.Point(633, 187);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(136, 38);
+            this.label5.TabIndex = 32;
+            this.label5.Text = "Время суток";
+            this.label5.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 30F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(937, 699);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.TimeBox);
+            this.Controls.Add(this.DayBox);
+            this.Controls.Add(this.MoneyBox);
+            this.Controls.Add(this.SaveButton);
+            this.Controls.Add(this.NextDayButton);
+            this.Controls.Add(this.CloseButton);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.HungerProgress);
+            this.Controls.Add(this.HealthProgress);
+            this.Controls.Add(this.UniversityButton);
+            this.Controls.Add(this.SchoolButton);
+            this.Controls.Add(this.button13);
+            this.Controls.Add(this.AnalystButton);
+            this.Controls.Add(this.VehiclesButton);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.SupermarketDButton);
+            this.Controls.Add(this.CassierButton);
+            this.Controls.Add(this.RewriterButton);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.HouseDButton);
+            this.Controls.Add(this.HouseCButton);
+            this.Controls.Add(this.SupermarketCButton);
+            this.Cursor = System.Windows.Forms.Cursors.Default;
+            this.Font = new System.Drawing.Font("Segoe Print", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.MaximizeBox = false;
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "SimForms v0.1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button SupermarketCButton;
+        private System.Windows.Forms.Button HouseCButton;
+        private System.Windows.Forms.Button HouseDButton;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button RewriterButton;
+        private System.Windows.Forms.Button CassierButton;
+        private System.Windows.Forms.Button SupermarketDButton;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button VehiclesButton;
+        private System.Windows.Forms.Button AnalystButton;
+        private System.Windows.Forms.Button button13;
+        private System.Windows.Forms.Button SchoolButton;
+        private System.Windows.Forms.Button UniversityButton;
+        private System.Windows.Forms.ProgressBar HealthProgress;
+        private System.Windows.Forms.ProgressBar HungerProgress;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button CloseButton;
+        private System.Windows.Forms.Button NextDayButton;
+        private System.Windows.Forms.Button SaveButton;
+        private System.Windows.Forms.TextBox MoneyBox;
+        private System.Windows.Forms.TextBox DayBox;
+        private System.Windows.Forms.TextBox TimeBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ MainForm.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SimForms
+{
+    public partial class MainForm : Form
+    {
+        private void SaveGame()
+        {
+            Properties.Settings save = new Properties.Settings
+            {
+                health = HealthProgress.Value,
+                hunger = HungerProgress.Value,
+                money = Convert.ToInt32(MoneyBox.Text),
+                day = Convert.ToInt32(DayBox.Text),
+                time = TimeBoxValue()
+            };
+            save.Save();
+        }
+        private void LoadGame()
+        {
+            Properties.Settings loadsaves = new Properties.Settings();
+            HealthProgress.Value = loadsaves.health;
+            HungerProgress.Value = loadsaves.hunger;
+            MoneyBox.Text = loadsaves.money.ToString();
+            DayBox.Text = loadsaves.day.ToString();
+            TimeBoxValue(loadsaves.time);
+        }
+
+        private int TimeBoxValue()
+        {
+            if (TimeBox.Text == "Ночь") return 0;
+            else if (TimeBox.Text == "Утро") return 1;
+            else if (TimeBox.Text == "День") return 2;
+            else if (TimeBox.Text == "Вечер") return 3;
+            else return 0;
+        }
+        private void TimeBoxValue(int type)
+        {
+            switch (type)
+            {
+                case 0:
+                    TimeBox.Text = "Ночь";
+                    break;
+                case 1:
+                    TimeBox.Text = "Утро";
+                    break;
+                case 2:
+                    TimeBox.Text = "День";
+                    break;
+                case 3:
+                    TimeBox.Text = "Вечер";
+                    break;
+            }
+        }
+
+        public MainForm()
+        {
+            InitializeComponent();
+            LoadGame();
+        }
+
+        private void HouseDButton_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void HouseCButton_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void VehiclesButton_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void SaveButton_Click(object sender, EventArgs e)
+        {
+            SaveGame();
+        }
+
+        private void SupermarketCButton_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void CloseButton_Click(object sender, EventArgs e)
+        {
+            var close = MessageBox.Show(null, "Весь Ваш прогресс будет удален, если вы не сохранились.\nВы уверены, что хотите закрыть программу?", "", MessageBoxButtons.YesNo, MessageBoxIcon.Warning, MessageBoxDefaultButton.Button2);
+            if(close == DialogResult.Yes)
+            {
+                this.Close();
+            }
+        }
+
+        private void NextDayButton_Click(object sender, EventArgs e)
+        {
+            if (TimeBoxValue() != 3) TimeBoxValue(TimeBoxValue() + 1);
+            else
+            {
+                TimeBoxValue(0);
+                DayBox.Text = (Convert.ToInt32(DayBox.Text) + 1).ToString();
+            }
+            SaveGame();
+        }
+
+    }
+}
